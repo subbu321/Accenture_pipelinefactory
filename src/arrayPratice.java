f4c62710a6d1493d6cdae849a1e09e2ac7f5a518
subbu321 <subbu.gtn317@gmail.com>
2022-12-25 10:00:47 +0530

    Array practice file

---
+++ src/arrayPratice.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class arrayPratice {
+
+	public static void main(String[] args) {
+        ArrayList<Integer> i =new ArrayList<Integer>();
+        i.add(20);
+        i.add(28);
+        i.add(22);
+      //  i.set(0,28); //values modified in present array index
+        //System.out.println(i.get(0));
+       // i.remove(1);  //remove values from mentioned index
+        System.out.println(i.get(1));
+        Collections.sort(i);
+    for (Integer integer : i) {
+		System.out.println(integer);
+	} 
+    ArrayList<String> s=new ArrayList<String>();
+    s.add("cars");
+    s.add("bull");
+    s.add("bike");
+    s.add("ninja");
+    s.add("car");
+    s.add("call");
+    Collections.sort(s);
+    for (String string : s) {
+		System.out.println(string);
+    }
+		HashMap<String,String> cc=new HashMap<String,String>();
+		cc.put("subbu", "342");
+		System.out.println(cc);
+	
+	}
+
+}
